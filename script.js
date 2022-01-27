a71ff3977c2915895eef4ec866ffcd6f820e598d
Bartosz69 <59618624+Bartosz69@users.noreply.github.com>
2022-01-27 18:17:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$('nav button').click(function () {
+	var SubPage = $(this).data("page");
+	$('nav button').removeClass("active");
+	$(this).addClass("active");
+	$.ajax({
+		url: "content/" + SubPage + ".html",
+		dataType: "html",
+		cache: false,
+		success: function (REPLY) {
+			$('section').html(REPLY);
+		},
+		error: function (x, errDetails) {
+			alert("ERROR: " + errDetails);
+		}
+		});
+});
+
+$('nav button[data-page="homepage"]').addClass("active");
+$('section').load("content/homepage.html");
